bb10b05926d38d538e76599d2aa0c8dcdd1ca891
Reymark cangque <50134483+ReymarkxD@users.noreply.github.com>
2019-05-16 23:51:34 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <nav>
+    <ul>
+      <li style="float: left;"><a href="#">Home</a></li>
+      <li><a href="#">Records</a></li>
+      <li><a href="Register.php">Register</a></li>
+      <li><a href="#">Log-out</a></li>
+    
+    </ul>
+  </nav>
+    
+  
+
+</body>
+</html>
